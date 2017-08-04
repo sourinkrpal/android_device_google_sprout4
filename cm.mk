a4d10f9031df474b79086f52fcd23a97d39f76ea
Samar Vispute <samarvispute121@gmail.com>
2017-08-04 15:34:43 +0530

    XOSP-Bringup

--- cm.mk
+++ cm.mk
@@ -1,11 +1,18 @@
 $(call inherit-product, device/google/sprout4/full_sprout4.mk)
 
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common XOSP stuff.
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout4
+PRODUCT_NAME := xosp_sprout4
 PRODUCT_DEVICE :=sprout4
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
@@ -13,3 +20,7 @@ PRODUCT_MODEL := A1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="sprout4"
+
+ROOT_METHOD := magisk
+#RELEASE_TYPE := XOSP_OFFICIAL
+USE_CHROMIUM := true
